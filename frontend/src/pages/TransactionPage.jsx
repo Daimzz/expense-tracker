39f33f7b8752e159bfb277f5a79f04b88a0737b5
Dmitry Moiseev <shanksdaymos@gmail.com>
2024-06-23 12:28:33 +0300

    fixed bug in transactionpage about refetching issues

--- frontend/src/pages/TransactionPage.jsx
+++ frontend/src/pages/TransactionPage.jsx
@@ -1,7 +1,7 @@
 import {useEffect, useState} from "react";
 import {useNavigate, useParams} from "react-router-dom";
 import {useMutation, useQuery} from "@apollo/client";
-import {GET_TRANSACTION} from "../graphql/queries/transaction.query.js";
+import {GET_TRANSACTION, GET_TRANSACTION_STATISTICS} from "../graphql/queries/transaction.query.js";
 import {UPDATE_TRANSACTION} from "../graphql/mutations/transaction.mutation.js";
 import toast from "react-hot-toast";
 import TransactionFormSkeleton from "../components/skeletons/TransactionFormSkeleton.jsx";
@@ -12,9 +12,11 @@ const TransactionPage = () => {
 	const {loading, data} = useQuery(GET_TRANSACTION, {
 		variables: {id},
 	})
-	const [updateTransaction, {loading:loadingUpdate}] = useMutation(UPDATE_TRANSACTION, {
-		refetchQueries: ['GetTransactions','GetTransactionStatistics']
-	})
+
+	const [updateTransaction, { loading: loadingUpdate }] = useMutation(UPDATE_TRANSACTION, {
+		refetchQueries: [{ query: GET_TRANSACTION_STATISTICS }],
+	});
+
 	const [formData, setFormData] = useState({
 		description:data?.transaction?.description || "",
 		paymentType: data?.transaction?.paymentType || "",
